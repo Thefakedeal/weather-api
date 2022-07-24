ec90e543b7e53e1a57c1c4136e658a570ec671a9
Samir Shrestha <theonlysamir@gmail.com>
2022-07-25 00:54:51 +0545

    #3 Weather model and migration added

---
+++ database/migrations/2022_07_24_190029_create_weather_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weather', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->json('new_york')->nullable();
+            $table->json('london')->nullable();
+            $table->json('paris')->nullable();
+            $table->json('berlin')->nullable();
+            $table->json('tokyo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weather');
+    }
+};
